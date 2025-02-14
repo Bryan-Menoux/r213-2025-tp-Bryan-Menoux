4365e0d92d9891634b0ea0ccf70a76504b7f9492
Bryan-Menoux <bryan.menoux@edu.univ-fcomte.fr>
2025-02-14 14:56:32 +0100

    fin de la partie filtre

--- src/backend.mjs
+++ src/backend.mjs
@@ -173,3 +173,24 @@ export async function addOffre(house) {
     };
   }
 }
+
+export async function filterByPrix(prixMin, prixMax) {
+  await superUserauth();
+  try {
+    let data = await pb.collection("maison").getFullList({
+      sort: "-date_de_creation",
+      filter: `prix >= ${prixMin} && prix <= ${prixMax}`,
+    });
+    data = data.map((maison) => {
+      maison.imgUrl = pb.files.getURL(maison, maison.images);
+      return maison;
+    });
+    return data;
+  } catch (error) {
+    console.log(
+      "Une erreur est survenue en filtrant la liste des maisons",
+      error
+    );
+    return [];
+  }
+}
