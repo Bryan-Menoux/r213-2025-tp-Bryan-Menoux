7f7c6678460b327d33b1f7ea64d39c16aa9c6f9e
Bryan-Menoux <bryan.menoux@edu.univ-fcomte.fr>
2025-02-14 14:01:24 +0100

    Fusion des tp
    Suite à un problème lié à github, mes commits n'étaient plus faits sur le repository des tps mais un repository personnel. J'ai modifié tout cela pour que cela soit dans le repository du tp

--- src/backend.mjs
+++ src/backend.mjs
@@ -1,7 +1,15 @@
 import PocketBase from "pocketbase";
 const pb = new PocketBase("http://127.0.0.1:8090");
 
+export async function superUserauth() {
+  const authData = await pb
+    .collection("_superusers")
+    .authWithPassword("admin@admin.com", "RU_D0f52Zwl4s2m");
+  return authData;
+}
+
 export async function allMaisons() {
+  await superUserauth();
   let records = await pb.collection("maison").getFullList();
   records = records.map((maison) => {
     maison.imgUrl = pb.files.getURL(maison, maison.images);
@@ -11,11 +19,13 @@ export async function allMaisons() {
 }
 
 export async function oneID(id) {
+  await superUserauth();
   const record = await pb.collection("maison").getOne(id);
   return record;
 }
 
 export async function allMaisonsFavori() {
+  await superUserauth();
   const record = await pb
     .collection("maison")
     .getFullList({ filter: "favori = true" });
@@ -23,11 +33,13 @@ export async function allMaisonsFavori() {
 }
 
 export async function allMaisonsSorted() {
+  await superUserauth();
   const record = await pb.collection("maison").getFullList({ sort: "prix" });
   return record;
 }
 
 export async function bySurface(surface) {
+  await superUserauth();
   let records = await pb
     .collection("maison")
     .getFullList({ filter: `surface >= ${surface}` });
@@ -39,6 +51,7 @@ export async function bySurface(surface) {
 }
 
 export async function byPrice(prix) {
+  await superUserauth();
   let records = await pb
     .collection("maison")
     .getFullList({ filter: `prix <= ${prix}` });
@@ -50,6 +63,7 @@ export async function byPrice(prix) {
 }
 
 export async function priceBetween(prixMin, prixMax) {
+  await superUserauth();
   let records = await pb.collection("maison").getFullList({
     filter: `prix >= ${prixMin} && prix <= ${prixMax}`,
   });
@@ -61,6 +75,7 @@ export async function priceBetween(prixMin, prixMax) {
 }
 
 export async function surfaceORprice(surface, price) {
+  await superUserauth();
   const record = await pb.collection("maison").getFullList({
     filter: `surface >= ${surface} || prix <= ${price}`,
   });
@@ -68,12 +83,15 @@ export async function surfaceORprice(surface, price) {
 }
 
 export async function donneesAgent(id) {
+  await superUserauth();
   const record = await pb.collection("agent").getOne(id);
   return record;
 }
 
 export async function fourchette(min, max) {
-  //  fonction ajoutée pour trier les maison ayant pour prix un intervalle donné. Les données sont triés par ordre croissant de prix
+  await superUserauth();
+  // Fonction ajoutée pour trier les maisons ayant pour prix un intervalle donné.
+  // Les données sont triées par ordre croissant de prix.
   const record = await pb
     .collection("maison")
     .getFullList({ sort: "prix", filter: `prix >= ${min} && prix <= ${max}` });
@@ -81,6 +99,7 @@ export async function fourchette(min, max) {
 }
 
 export async function getOffre(id) {
+  await superUserauth();
   try {
     let maison = await pb.collection("maison").getOne(id);
     maison.imgUrl = pb.files.getURL(maison, maison.images);
@@ -90,4 +109,49 @@ export async function getOffre(id) {
     return null;
   }
 }
-// test 
+
+// Test
+export async function addNewMaison(newMaison) {
+  await superUserauth();
+  await pb.collection("maison").create(newMaison);
+}
+
+export async function newAgent(newAgent) {
+  await superUserauth();
+  await pb.collection("agent").create(newAgent);
+}
+
+export async function deleteMaisonById(id) {
+  await superUserauth();
+  await pb.collection("maison").delete(id);
+}
+
+export async function deleteAgentById(id) {
+  await superUserauth();
+  await pb.collection("agent").delete(id);
+}
+
+export async function updateMaisonById(id, data) {
+  await superUserauth();
+  await pb.collection("maison").update(id, data);
+}
+
+export async function updateAgentById(id, data) {
+  await superUserauth();
+  await pb.collection("agent").update(id, data);
+}
+
+// TP 6
+
+export async function addNewUser(user) {
+  await superUserauth();
+  await pb.collection("users").create(user);
+  pb.authStore.clear();
+}
+
+export async function userAuth(login, mdp) {
+  await superUserauth();
+  const authData = await pb.collection("users").authWithPassword(login, mdp);
+  pb.authStore.clear();
+  return authData;
+}
